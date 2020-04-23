f9fdd9801aefccf7418dd63cc4fffefc57d788d7
cmoyano <cristianmoyano.mza@gmail.com>
2020-04-22 23:11:49 -0300

    add monitoring

--- server/test_settings.py
+++ server/test_settings.py
@@ -1,4 +1,7 @@
 from server.settings import *
 
 SOUTH_TESTS_MIGRATE = False
-DATABASES['default'] = DATABASES['sqlite3']
+DATABASES['default'] = {
+    'ENGINE': 'django.db.backends.sqlite3',
+    'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+}
--- server/urls.py
+++ server/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('', include('django_prometheus.urls')),
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('api/auth/', include('accounts.api.urls')),
